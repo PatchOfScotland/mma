7875da0a4ed1790457f71fb31bf723f7170a6ecb
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2024-12-06 08:44:15 +0100

    run script

---
+++ src/fut_programs/run_bench.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+echo "Starting autotune..."
+echo "--------------------"
+futmma autotune --backend=cuda attention_like_orig.fut
+futmma autotune --backend=cuda custom_attention_like_orig.fut
+futmma autotune --backend=cuda batched_mmm_orig.fut
+echo "--------------------"
+echo "Autotune done..."
+echo "Starting bench..."
+echo "--------------------"
+futmma bench --backend=cuda attention_like_orig.fut
+futmma bench --backend=cuda custom_attention_like_orig.fut
+futmma bench --backend=cuda batched_mmm_orig.fut
+echo "--------------------"
+echo "Bench done..."
+echo "Starting bench tensor cores..."
+echo "--------------------"
+futmma bench --backend=cudatc --pass-option=--nvrtc-option=-i../../cutlass/include attention_like.fut
+futmma bench --backend=cudatc --pass-option=--nvrtc-option=-i../../cutlass/include custom_attention_like.fut
+futmma bench --backend=cudatc --pass-option=--nvrtc-option=-i../../cutlass/include batched_mmm.fut
