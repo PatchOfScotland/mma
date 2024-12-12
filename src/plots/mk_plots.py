5bf32c5ffbff550d2d24111aa15ff06e8f9a5b14
TheRune22 <rune.e.b.lejboelle@gmail.com>
2024-12-12 11:13:25 +0100

    Added batched mmm

--- src/plots/mk_plots.py
+++ src/plots/mk_plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def matmul():    
+def matmul():
     matmul_names = ["Baseline",
                     "Swizzling",
                     "Vectorized copies,\nglobal to shared",
@@ -34,7 +34,7 @@ def lud_like():
     time_us_f32 = np.array([783, 1227, 2001,12705])
     # time_us_f32 = np.array([1046, 6680, 68845, 1407986])
     total_ops = blocks * (ds * ds * ds * 2 + ds * ds)
-    tflops_tc = total_ops / (time_tc_us * 1_000_000)    
+    tflops_tc = total_ops / (time_tc_us * 1_000_000)
     tflops_orig_f16 = total_ops / (time_us_f32 * 1_000_000)
 
     plt.plot(tflops_tc, marker='o', linestyle='-', color='skyblue', label="CUDA backend + TC f16/f32")
@@ -55,8 +55,8 @@ def attention_like():
     time_tc_us = np.array([214, 557, 3981, 11064])
     time_us_f16 = np.array([14100, 21990, 35660, 129815])
     # time_us_f32 = np.array([1046, 6680, 68845, 1407986])
-    
-    tflops_tc = blocks * (ds * ds * ks) * 2 / (time_tc_us * 1_000_000)    
+
+    tflops_tc = blocks * (ds * ds * ks) * 2 / (time_tc_us * 1_000_000)
     tflops_orig_f16 = blocks * (ds * ds * ks) * 2 / (time_us_f16 * 1_000_000)
     # tflops_orig_f32 = blocks * (ds ** 3) * 2 * 2 / (time_us_f32 * 1_000_000)
 
@@ -78,7 +78,7 @@ def custom_attention():
     time_tc_us = np.array([382, 414, 2545, 8410])
     time_us_f16 = np.array([833, 5088, 47410, 505696])
     time_us_f32 = np.array([1046, 6680, 68845, 1407986])
-    
+
     tflops_tc = blocks * (ds ** 3) * 2 * 2 / (time_tc_us * 1_000_000)
     tflops_orig_f16 = blocks * (ds ** 3) * 2 * 2 / (time_us_f16 * 1_000_000)
     tflops_orig_f32 = blocks * (ds ** 3) * 2 * 2 / (time_us_f32 * 1_000_000)
@@ -96,8 +96,34 @@ def custom_attention():
     plt.legend()
     plt.tight_layout()
     plt.show()
-    
-lud_like()    
-attention_like()    
-matmul()
-custom_attention()
+
+def batched_mmm():
+    blocks = 8192
+    ds = np.array([16, 32, 64, 128])
+    time_tc_us = np.array([38, 74, 237, 822])
+    time_us_f16 = np.array([334, 525, 844, 5164])
+    time_us_f32 = np.array([334, 589, 1031, 6031])
+
+    tflops_tc = blocks * (ds ** 3) * 2 / (time_tc_us * 1_000_000)
+    tflops_orig_f16 = blocks * (ds ** 3) * 2 / (time_us_f16 * 1_000_000)
+    tflops_orig_f32 = blocks * (ds ** 3) * 2 / (time_us_f32 * 1_000_000)
+
+    # plt.axhline(124, color="r", label="FlashAttention")
+    plt.plot(ds, tflops_tc, marker='o', linestyle='-', color='skyblue', label="CUDA backend + TC f16")
+    plt.plot(ds, tflops_orig_f16, marker='o', linestyle='-', color='b', label="CUDA backend f16")
+    plt.plot(ds, tflops_orig_f32, marker='o', linestyle='-', color='lightcoral', label="CUDA backend f32")
+    plt.title("Batched matrix multiplication")
+    plt.xlabel("Matrix size $n$ of $n\\times n \\times n$ matrix multiplication")
+    plt.ylabel("TFLOPS")
+    # plt.xticks(range(len(time_tc_us)), labels=ds)
+    plt.grid(alpha=0.5)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+# lud_like()
+# attention_like()
+# matmul()
+# custom_attention()
+batched_mmm()
